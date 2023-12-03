93a83fbdd8abd9871b72864239051f84605d112f
AndressaOL <eng.agro.andressa@gmail.com>
2023-12-03 09:23:24 -0800

    Update FRiP_score.sh

--- Pipelines/FRiP_score.sh
+++ Pipelines/FRiP_score.sh
@@ -15,7 +15,6 @@ samtools view -c  $1 > $1_samtools.txt
 
 ## Second Step
 ##convert BAM (BAM used to call peaks) to BED
-
 bedtools bamtobed -i $1 | awk 'BEGIN{OFS="\t"}{$4="N";$5="1000";print $0}'  > $1.tagAlign.bed
 bedtools sort -i  $1.tagAlign.bed >   $1.sorted.tagAlign.bed 
 
@@ -25,7 +24,6 @@ bedtools sort -i $2 | bedtools merge -i stdin |\
 bedtools intersect -u -a $1.sorted.tagAlign.bed -b stdin | wc -l > $2_total.txt
 
 ##Calculate the FRiPs
- ##calculate  the FRiPs
 v1=$(<"$2_total.txt")
 v2=$(<"$1_samtools.txt")
  #result=$(($v1 / $v2))
